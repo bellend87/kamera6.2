5b7a427ac5f140c287b0df3f88036466460a7cef
Samuli <samuli.nybergh@gmail.com>
2020-02-06 14:10:25 +0200

    kameratesti

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var alkuarvot = {video:{facingMode: "user"}, audio: false};
 var track = null;
 
 var kamerakuva = document.getElementById("kamerakuva");
-var kuvasijainti = document.getElementById("kamersijainti");
+var kuvasijainti = document.getElementById("kuvasijainti");
 var kuvacanvas = document.getElementById("kameracanvas");
 var laukaisin = document.getElementById("laukaisin");
 
@@ -10,6 +10,14 @@ function cameraStarttaa() {
     navigator.mediaDevices.getUserMedia(alkuarvot)
     .then(function(kuvavirta){ kamerakuva.srcObject = kuvavirta;})
     .catch(function(error){console.log("hupsista")})
+}
+
+laukaisin.onclick = function() {
+    kameracanvas.width = kamerakuva.videoWidth;
+    kameracanvas.height = kamerakuva.videoHeight;
+    kameracanvas.getContext("2d").drawImage(kamerakuva, 0,0);
+    kuvasijainti.src = kameracanvas.toDataURL ("image/png");
+    kuvasijainti.id = "otto";
 }
 
 window.addEventListener("load", cameraStarttaa);
